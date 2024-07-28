c02a9eff9bdaed7d4fede72dddc29281fbd03a09
vwh <vwhe@proton.me>
2024-07-28 20:14:32 +0300

    fixing theme toggle

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -17,9 +17,9 @@ export default function ModeToggle() {
       className="relative flex items-center justify-center p-2 transition-transform duration-300 hover:bg-gray-100 dark:hover:bg-gray-800"
     >
       {theme === "dark" ? (
-        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <SunIcon className="h-[1.2rem] w-[1.2rem]" />
       ) : (
-        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
       )}
     </Button>
   );
