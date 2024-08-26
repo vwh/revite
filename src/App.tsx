b173d6ac0315cf46f4c1a9ad86e66f24d749536d
vwh <vwhe@proton.me>
2024-08-26 04:18:13 +0300

    added theme folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Image from "@/components/image";
-import ModeToggle from "@/components/mode-toggle";
+import ModeToggle from "@/components/theme/theme-toggle";
 import { Button } from "./components/ui/button";
 import { GithubIcon } from "lucide-react";
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
-import ThemeProvider from "@/components/theme-provider";
+import ThemeProvider from "@/components/theme/theme-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
