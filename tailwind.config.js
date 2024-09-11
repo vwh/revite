956beb2a5a1d4dd1577e5bc94ee5e7631f7dcc55
vwh <vwhe@proton.me>
2024-09-11 21:29:53 +0300

    feat: added index.html on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ export default {
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}"
+    "./src/**/*.{ts,tsx}",
+    "index.html"
   ],
   prefix: "",
   theme: {
